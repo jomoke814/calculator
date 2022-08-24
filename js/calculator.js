79b401082a072a67d004aba7747a6d8fea6aa524
jomoke814 <jcullen814@gmail.com>
2022-08-24 19:01:27 +0300

    Adding html and css styles on backspace, power button and js eventlistener

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,11 @@
-console.log("Hello!")
+const calcInput = document.querySelector('.screen')
+const buttons = document.querySelectorAll('.button')
+// const operators = ['*', '/', '+', '-']
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.dataset.value
+        calcInput.innerText += value
+    })
+})
+
